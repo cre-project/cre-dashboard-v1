8968cc39a2d7171543add098284cc268df556c6b
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2018-06-28 15:52:10 +0200

    Unit Mix WIP

---
+++ src/components/Unit.vue
@@ -0,0 +1,48 @@
+<template>
+  <tr class="editable">
+      <td>
+          <select class="buttonize down-arrow">
+              <option>1 Bedroom & 1 Bathroom</option>
+              <option>1 Bedroom & 2 Bathroom</option>
+              <option>2 Bedroom & 1 Bathroom</option>
+          </select>
+      </td>
+      <td><input class="inline-edit" disabled v-model="unit.squareFeet" value="3650"></td>
+      <td>$<input class="inline-edit" disabled v-model="unit.currentRent" value="2000"></td>
+      <td>$<input class="inline-edit" disabled v-model="unit.potentialRent" value="3000"></td>
+  </tr>
+</template>
+<script>
+// import { mapActions, mapState } from 'vuex'
+// import router from '../router/index'
+
+export default {
+  props: {
+    unit: {
+      type: Object,
+      required: true
+    }
+  //   id: {
+  //     type: String,
+  //     required: true
+  //   }
+  // },
+  // computed: {
+  //   ...mapState({
+  //     current: state => state.valuations.currentId,
+  //     wip: state => state.valuations.wip,
+  //     isEditing: state => state.valuations.isEditing
+  //   })
+  // },
+  // methods: {
+  //   ...mapActions('valuations', ['setWip', 'toggleEditing']),
+  //   editValuation () {
+  //     this.setWip({valuation: this.valuation, id: this.id})
+  //     this.toggleEditing()
+  //     router.push('/property-info')
+  //   }
+  }
+}
+</script>
+<style scoped>
+</style>
--- src/routes/UnitMix.vue
+++ src/routes/UnitMix.vue
@@ -1,9 +1,46 @@
 <template>
     <div>
         <navigation-header selected="unit-mix"></navigation-header>
+        <div class="wrapper">
+            <h1>Unit Mix</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Unit Type</th>
+                        <th>Square Feet</th>
+                        <th>Current Rent/Mo.</th>
+                        <th>Potential Rent/Mo.</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <unit v-for="unit in units" :key="unit" :unit="unit"></unit>
+                </tbody>
+            </table>
+            <div id="add-more">
+                <div class="add-more-content" id="plus">+</div>
+                <span class="add-more-content">Add Another Unit</span>
+            </div>
+            <button class="save" type="submit" @click="save">Save & Next</button>
+        </div>
     </div>
 </template>
 <script>
+import router from '../router/index'
+import Unit from '@/components/Unit'
+
 export default {
+  data () {
+    return {
+      units: []
+    }
+  },
+  methods: {
+    save () {
+      router.push('/operating-statement')
+    }
+  },
+  components: {
+    Unit: Unit
+  }
 }
 </script>
