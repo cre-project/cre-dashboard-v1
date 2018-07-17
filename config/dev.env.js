859a4451e86936a3f3558554b0e59eb876d39be2
Unknown <sjeftijssen@gmail.com>
2018-07-17 14:08:02 +0200

    Added API Key

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,7 +5,7 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   FIREBASE: {
-    apiKey: '"<add-api-key-here>"',
+    apiKey: '"AIzaSyCggcOompCU9QYwYXxbdOSHbge6QDoNspI"',
     authDomain: '"cre-dashboard.firebaseapp.com"',
     databaseURL: '"https://cre-dashboard.firebaseio.com"',
     projectId: '"cre-dashboard"',
