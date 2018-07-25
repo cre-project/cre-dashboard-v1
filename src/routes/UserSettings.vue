ed522682c2eae1e58d2d092b925dd9015669cb75
Monika Kedrova <6909854+m-ke@users.noreply.github.com>
2018-07-25 18:56:40 +0200

    User profile picture and org logo upload

--- src/routes/UserSettings.vue
+++ src/routes/UserSettings.vue
@@ -26,7 +26,7 @@
                 <input v-model="user.phoneNumber">
             </label>
             <label>
-                <div>Add Profile Picture</div>
+                <div>Profile Picture</div>
                 <img class="hidden" id="profile-preview">
                 <input type="file" class="save hidden" @input="loadProfilePic">
                 <i class="large material-icons clickable" id="profile-icon">add_a_photo</i>
@@ -47,7 +47,7 @@
                 <input v-model="user.email">
             </label>
             <label>
-                <div>Add Company Logo</div>
+                <div>Company Logo</div>
                 <img class="hidden" id="logo-preview" title="click to change picture">
                 <input type="file" class="save hidden" @input="loadLogo">
                 <i class="large material-icons clickable" id="logo-icon">add_a_photo</i>
@@ -59,7 +59,7 @@
 <script>
 import { mapActions, mapState } from 'vuex'
 import router from '../router/index'
-// import { upload } from '../store/tools/images'
+import { upload, getUrl } from '../store/tools/images'
 
 export default {
   data () {
@@ -69,9 +69,6 @@ export default {
   },
   methods: {
     ...mapActions('users', ['set']),
-    ...mapState({
-      userId: state => state.users.currentId
-    }),
     save () {
       this.set(this.user)
       router.push('./valuations')
@@ -79,6 +76,7 @@ export default {
     loadNewImage (previewEl, button, imgName, evt) {
       let file = evt.target.files[0]
       let reader = new FileReader()
+      let fileName = `images/${imgName}`
 
       reader.addEventListener('load', function (evt) {
         previewEl.src = evt.target.result
@@ -86,26 +84,50 @@ export default {
         previewEl.classList.add('clickable')
         button.classList.remove('clickable')
         button.classList.add('hidden')
-        // upload(fileName, evt.target.result)
+        upload(fileName, evt.target.result)
       })
       reader.readAsDataURL(file)
     },
+    loadExistingImage (previewEl, button, url) {
+      previewEl.src = url
+      previewEl.classList.remove('hidden')
+      previewEl.classList.add('clickable')
+      button.classList.remove('clickable')
+      button.classList.add('hidden')
+    },
     loadProfilePic (evt) {
-      const preview = document.querySelector('#profile-preview')
-      const icon = document.querySelector('#profile-icon')
-
-      this.loadNewImage(preview, icon, 'profile.jpg', evt)
+      const profilePreview = document.querySelector('#profile-preview')
+      const profileIcon = document.querySelector('#profile-icon')
+      let fileName = `${this.userId}/profile.png`
+      this.loadNewImage(profilePreview, profileIcon, fileName, evt)
     },
     loadLogo (evt) {
-      const preview = document.querySelector('#logo-preview')
-      const icon = document.querySelector('#logo-icon')
-
-      this.loadNewImage(preview, icon, 'logo.jpg', evt)
+      const logoPreview = document.querySelector('#logo-preview')
+      const logoIcon = document.querySelector('#logo-icon')
+      let fileName = `${this.userId}/logo.png`
+      this.loadNewImage(logoPreview, logoIcon, fileName, evt)
     }
+  },
+  computed: {
+    ...mapState({
+      userId: state => state.users.currentId
+    })
   },
   created () {
     // fill up the local user object
     this.user = this.$store.state.users.currentUser
+    let vm = this
+    // load logo and preview if they exist
+    getUrl(`images/${this.$store.state.users.currentId}/profile.png`).then(downloadUrl => {
+      if (downloadUrl) {
+        vm.loadExistingImage(document.querySelector('#profile-preview'), document.querySelector('#profile-icon'), downloadUrl)
+      }
+    })
+    getUrl(`images/${this.$store.state.users.currentId}/logo.png`).then(downloadUrl => {
+      if (downloadUrl) {
+        vm.loadExistingImage(document.querySelector('#logo-preview'), document.querySelector('#logo-icon'), downloadUrl)
+      }
+    })
   }
 }
 </script>
---
+++ src/store/tools/images.js
@@ -0,0 +1,25 @@
+import firebase from 'firebase/app'
+import 'firebase/storage'
+
+export function upload (fileName, file) {
+  console.log('Uploading ...')
+  let ref = firebase.storage().ref()
+  let uploadTask = ref.child(fileName).putString(file, 'data_url')
+
+  return uploadTask.then(function (snapshot) {
+    // Upload completed successfully, now we can get the download URL
+    return snapshot.ref.getDownloadURL().then(function (downloadURL) {
+      console.log('Uploaded image to', downloadURL)
+      return downloadURL
+    })
+  }).catch(error => console.error(error.code))
+}
+
+export function getUrl (fileName) {
+  let ref = firebase.storage().ref().child(fileName)
+
+  return ref.getDownloadURL().then(url => { return url }).catch(e => {
+    console.error(e.code)
+    return null
+  })
+}
